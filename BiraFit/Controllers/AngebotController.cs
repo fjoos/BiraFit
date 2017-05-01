b3d5da222e07e2e82bd16fc22e514e8d5529dfe0
Pekent Durmaz <pekent.durmaz@gmail.com>
2017-05-01 20:14:58 +0200

    AngebotController Refactoring

--- BiraFit/Controllers/AngebotController.cs
+++ BiraFit/Controllers/AngebotController.cs
@@ -15,24 +15,12 @@ namespace BiraFit.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
-            var bedarfList = Context.Bedarf.ToList();
+            
             int currentSportlerId = AuthentificationHelper.AuthenticateSportler(User, Context).Id;
-            List<Angebot> angebote = new List<Angebot>();
-
-                foreach (var bedarf in bedarfList)
-                {
-                    if(bedarf.Sportler_Id == currentSportlerId && bedarf.OpenBedarf)
-                    {
-                        foreach (var angebot in Context.Angebot.ToList())
-                        {
-                            if(bedarf.Id == angebot.Bedarf_Id)
-                            {
-                                angebote.Add(angebot);
-                            }
-                        }
-                    }
-                }
-                return View(angebote);
+            var currentBedarf = Context.Bedarf.Single(s => s.Sportler_Id == currentSportlerId && s.OpenBedarf);
+            List<Angebot> angebotList = new List<Angebot>(Context.Angebot.Where(b => b.Bedarf_Id == currentBedarf.Id));
+           
+            return View(angebotList);
             
         }
     }
