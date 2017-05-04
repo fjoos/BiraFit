d83ada48a4ad2c92874e4e182d955667cd7220b9
Pekent Durmaz <pekent.durmaz@gmail.com>
2017-05-04 10:47:46 +0200

    New Migration with Field Beschreibung and Username to DB and Adress nullable

--- BiraFit/ViewModel/BedarfViewModel.cs
+++ BiraFit/ViewModel/BedarfViewModel.cs
@@ -10,5 +10,6 @@ namespace BiraFit.ViewModel
         public Bedarf Bedarf { get; set; }
         public bool IsOwner { get; set; }
         public bool OfferMade { get; set; }
+        public Angebot Angebot { get; set; }
     }
 }
