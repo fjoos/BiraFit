1cb947d7e3aabe79e535a745d42e72c0ed3a9021
fjoos <fabjoos@gmail.com>
2017-05-20 13:00:46 +0200

    Tests

--- BiraFitTests/Controllers/AccountControllerTests.cs
+++ BiraFitTests/Controllers/AccountControllerTests.cs
@@ -2,6 +2,10 @@
 using System;
 using BiraFit.Models;
 using System.Web.Mvc;
+using System.Web;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace BiraFit.Controllers.Tests
 {
@@ -9,13 +13,86 @@ namespace BiraFit.Controllers.Tests
     public class AccountControllerTests
     {
         private AccountController _controller;
-
+        private Mock<HttpContextBase> moqContext;
+        private Mock<HttpRequestBase> moqRequest;
 
         [TestInitialize]
         public void TestInitialize()
         {
             _controller = new AccountController();
+            moqContext = new Mock<HttpContextBase>();
+            moqRequest = new Mock<HttpRequestBase>();
         }
+
+        [TestMethod()]
+        public void RegisterSportler()
+        {
+            var model = new RegisterViewModel() {
+                Email = "birfafit17@gmail.com",
+                Firstname = "Bira",
+                Lastname = "Fit",
+                Birthdate = new DateTime(1999,1,1),
+                Password = "Hsr-123",
+                ConfirmPassword = "Hsr-123",
+                Sportler = true };
+
+            ValidateModel(model, _controller);
+
+            var asfd = _controller.Register(model);
+            Assert.IsNotNull(asfd.);
+        }
+
+        private static void ValidateModel(object model, Controller controller)
+        {
+            var context = new ValidationContext(model);
+            var validationResults = new List<ValidationResult>();
+            Validator.TryValidateObject(model, context, validationResults, true);
+
+            foreach (var validationResult in validationResults)
+            {
+                controller.ModelState.AddModelError("CustomError", validationResult.ErrorMessage);
+            }
+        }
+
+        /*
+         * 
+         *  var model = new NewOrderViewModel() {Name = "Michael"};
+
+            var context = new DefaultHttpContext {User = CreateUser(), RequestServices = _serviceProvider};
+            
+            var controller = new OrderController(new OrderService(_dbContext, new FakeUserManager()));
+            controller.ControllerContext = new ControllerContext() {HttpContext = context};
+
+            var view = controller.Create(model);
+            Assert.IsType<PartialViewResult>(view);
+
+
+             if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    AnmeldeDatum = DateTime.Now,
+                    Name = model.Lastname,
+                    Vorname = model.Firstname,
+                    Aktiv = 1                                        
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    var msg = "Willkommen bei BiraFit! Bitte bestätigen Sie Ihr Konto. Klicken Sie dazu <a href=\"" + callbackUrl + "\">Email bestätigen</a>";
+                    var header = "Konto bestätigen";
+                    SendMail(user.Email, msg, header);
+
+                    AllocateUser(user, model);
+                    return RedirectToAction("Confirmation", "Account");
+                }
+                AddErrors(result);
+            }
+         * */
 
         [TestMethod()]
         public void LoginView()
